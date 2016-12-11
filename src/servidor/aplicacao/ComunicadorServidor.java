cce87796977f8725657636d2e6c6f29c9bf53b81
André Luiz <andreluiz.97@outlook.com>
2016-12-11 17:43:19 -0200

    Implementação de cadastro RMI e correções

--- src/servidor/aplicacao/ComunicadorServidor.java
+++ src/servidor/aplicacao/ComunicadorServidor.java
@@ -6,6 +6,8 @@ import compartilhado.modelo.Mensagem;
 import compartilhado.modelo.Usuario;
 import compartilhado.modelo.UsuarioAutenticacao;
 import java.awt.Image;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -18,7 +20,7 @@ public class ComunicadorServidor implements IComunicadorServidor {
     @Override
     public int autenticarUsuario(UsuarioAutenticacao autenticacao) throws RemoteException {
         /*  CÓDIGO DE RETORNO (STATUS)
-            -1 - Erro padrão
+            -1 - Erro genérico
             0 - Usuário não existe
             1 - Usuário já está online
             2 - Senha incorreta
@@ -31,28 +33,52 @@ public class ComunicadorServidor implements IComunicadorServidor {
                 existe = true;
                 //setIdCliente(u.getId());
                 if(u.isOnline())
-                    return 1; // já está online, então retorna 1
+                    return 1; // se já está online, então retorna 1
             }
         }
         if(!existe)
-            return 0;
+            return 0; // se usuário não existe, então retorna 0
         try {
-            status = Principal.gerenciador.autenticarUsuario(autenticacao);
+            status = Principal.gerenciador.autenticarUsuario(autenticacao); // tenta autenticar na db
         } catch (SQLException ex) {
             Principal.frmPrincipal.enviarLog("Exceção ao autenticar usuário " + autenticacao.getUsuario() + ": " + ex.getMessage());
             return status;
         }
         if(status == 2){
-            return status;
+            return status; // se a senha está incorreta, retorna
         }
-        Principal.frmPrincipal.enviarLog("Usuário " + autenticacao.getUsuario() + " se conectou");
-        Principal.frmPrincipal.alterarUsuarios(true);
-        return status;
+        Principal.frmPrincipal.enviarLog("Usuário " + autenticacao.getUsuario() + " se conectou"); // envia log de autenticação
+        Principal.frmPrincipal.alterarUsuarios(true); // incrementa número de usuários online
+        return status; // retorna status de autenticação
     }
 
     @Override
     public int cadastrarUsuario(UsuarioAutenticacao autenticacao) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /*  CÓDIGO DE RETORNO (STATUS)
+            -1 - Erro genérico
+            0 - Usuário já existe
+            1 - Cadastrou
+        */
+        int status = -1; // controle de status do cadastro
+        boolean cadastro; // controle para retorno da db
+        for (Usuario u : Principal.usuarios) { // verifica se o usuário já existe na lista de usuários
+            if(u.getUsuario().equals(autenticacao.getUsuario())){
+                return 0; // se usuário já existe, então retorna 0
+            }
+        }
+        try {
+            cadastro = Principal.gerenciador.cadastrarUsuario(autenticacao); // tenta cadastrar na db
+        } catch (SQLException | IOException | URISyntaxException ex) {
+            Principal.frmPrincipal.enviarLog("Exceção ao cadastrar usuário " + autenticacao.getUsuario() + ": " + ex.getMessage());
+            return status;
+        }
+        if(!cadastro){
+            return status; // se a db retornou falso, retorna o status
+        }else{
+            status = 1; // se não, status é 1 (cadastrou)
+        }
+        Principal.frmPrincipal.enviarLog("Usuário " + autenticacao.getUsuario() + " se cadastrou"); // envia log de cadastro
+        return status; // retorna o status
     }
 
     @Override
