7dfc18a75a1a7e2e864949ee2e56cf960e1a67ad
André Luiz <andreluiz.97@outlook.com>
2016-12-18 23:43:08 -0200

    Método de inicialização de jogo no servidor

--- src/servidor/aplicacao/Jogo.java
+++ src/servidor/aplicacao/Jogo.java
@@ -1,6 +1,7 @@
 package servidor.aplicacao;
 
 import compartilhado.modelo.Usuario;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 public class Jogo {
@@ -16,11 +17,35 @@ public class Jogo {
         this.idGrupo = idGrupo;
         this.timeAzul = timeAzul;
         this.timeVermelho = timeVermelho;
-        vidaAzul = 50;
-        vidaVermelho = 50;
+        inicializar();
     }
     
-    public void enviarInformacao(String informacao){
+    public void inicializar(){
+        vidaAzul = 50;
+        vidaVermelho = 50;
+        try {
+            enviarInformacao("Iniciando jogo...");
+            String azul = "Time azul: ";
+            for (Usuario usuario : timeAzul) {
+                azul += usuario.getUsuario() + ", ";
+            }
+            azul = azul.substring(0, azul.lastIndexOf(","));
+            String vermelho = "Time vermelho: ";
+            for (Usuario usuario : timeVermelho) {
+                vermelho += usuario.getUsuario() + ", ";
+            }
+            vermelho = vermelho.substring(0, vermelho.lastIndexOf(","));
+            enviarInformacao(azul);
+            enviarInformacao(vermelho);
+            enviarInformacao("Comandos:\n.cima\n.baixo\n.esquerda\n.direita\n.atirar");
+            enviarInformacao("O jogo foi iniciado");
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+            Principal.frmPrincipal.enviarLog("Falha ao enviar informação para grupo " + idGrupo + ":" + ex.getMessage());
+        }
+    }
+    
+    public void enviarInformacao(String informacao) throws RemoteException{
         for (Usuario usuario : timeAzul) {
             if(Principal.getConexaoPorIdUsuario(usuario.getId()) != null)
                 Principal.getConexaoPorIdUsuario(usuario.getId()).comunicador.receberInformacao(idGrupo, informacao);
