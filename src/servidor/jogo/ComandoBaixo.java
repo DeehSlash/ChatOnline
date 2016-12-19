c45e7b17b310ef78a250b67d9ee533c462f7f16f
André Luiz <andreluiz.97@outlook.com>
2016-12-19 11:13:30 -0200

    Correção na detecção de colisão

--- src/servidor/jogo/ComandoBaixo.java
+++ src/servidor/jogo/ComandoBaixo.java
@@ -21,14 +21,25 @@ public class ComandoBaixo implements Comando {
             posicao = jogo.getPosicaoAzul();
         else
             posicao = jogo.getPosicaoVermelho();
-        // verifica x
+        // verifica se vai passar dos limites
         if((posicao.y + jogo.getTamVeiculo() + jogo.getPasso()) > jogo.getTamJanela().y)
             return;
         // verifica colisão
-        if(((posicao.y + jogo.getTamVeiculo() + jogo.getPasso()) >= jogo.getPosicaoVermelho().y) &&
-                ((posicao.y + jogo.getTamVeiculo() + jogo.getPasso()) <= jogo.getPosicaoVermelho().y + jogo.getTamVeiculo()))
+        Point comparacao = new Point();
+        if(time.equals("azul")){
+            comparacao.x = jogo.getPosicaoVermelho().x;
+            comparacao.y = jogo.getPosicaoVermelho().y;
+        }else if(time.equals("vermelho")){
+            comparacao.x = jogo.getPosicaoAzul().x;
+            comparacao.y = jogo.getPosicaoAzul().y;
+        }
+        boolean colisaoVertical = ((posicao.y + jogo.getTamVeiculo() + jogo.getPasso()) >= comparacao.y) &&
+                ((posicao.y + jogo.getTamVeiculo() + jogo.getPasso()) <= (comparacao.y + jogo.getTamVeiculo()));
+        boolean colisaoHorizontal = ((posicao.x + jogo.getTamVeiculo() + jogo.getPasso()) >= comparacao.x) &&
+                ((posicao.x + jogo.getTamVeiculo() + jogo.getPasso()) <= (comparacao.x + jogo.getTamVeiculo()));
+        if(colisaoVertical && colisaoHorizontal)
             return;
-        posicao.y += jogo.getPasso(); // incrementa o x (anda para baixo)
+        posicao.y += jogo.getPasso(); // incrementa o y (anda para baixo)
         if(time.equals("azul")) // seta a posição no jogo
             jogo.setPosicaoAzul(posicao);
         else
