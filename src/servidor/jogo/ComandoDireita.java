673238845ee87508e58cc6f2a52940f4374876c0
André Luiz <andreluiz.97@outlook.com>
2016-12-19 11:55:35 -0200

    Implementado Comando Direita e Esquerda

--- src/servidor/jogo/ComandoDireita.java
+++ src/servidor/jogo/ComandoDireita.java
@@ -1,5 +1,8 @@
 package servidor.jogo;
 
+import java.awt.Point;
+import servidor.aplicacao.Principal;
+
 public class ComandoDireita implements Comando {
 
     int id;
@@ -12,7 +15,29 @@ public class ComandoDireita implements Comando {
     
     @Override
     public void executar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Jogo jogo = Principal.getJogo(id);
+        Point posicao;
+        if(time.equals("azul"))
+            posicao = jogo.getPosicaoAzul();
+        else
+            posicao = jogo.getPosicaoVermelho();
+        // verifica se vai passar dos limites
+        if((posicao.x + jogo.getTamVeiculo() + jogo.getPasso()) > jogo.getTamJanela().x)
+            return;
+        // verifica colisão
+        if(time.equals("azul")){
+            if(jogo.getLimite("azul", jogo.getPasso(), 0).intersects(jogo.getLimite("vermelho", 0, 0)))
+                return;
+        }else{
+            if(jogo.getLimite("azul", 0, 0).intersects(jogo.getLimite("vermelho", jogo.getPasso(), 0)))
+                return;
+        }
+        posicao.x += jogo.getPasso(); // incrementa o y (anda para baixo)
+        if(time.equals("azul")) // seta a posição no jogo
+            jogo.setPosicaoAzul(posicao);
+        else
+            jogo.setPosicaoVermelho(posicao);
+        jogo.atualizarPosicao(time); // atualiza todos os jogadores
     }
 
 }
